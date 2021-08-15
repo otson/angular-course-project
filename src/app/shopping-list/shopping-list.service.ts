802894517a8d100a43ab0544b4022be8d807c7aa
otson <59139705+otson@users.noreply.github.com>
2021-08-15 10:34:01 +0300

    move ingredients to shopping list service

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -1,6 +1,14 @@
 import {Injectable} from "@angular/core";
+import {Ingredient} from "../shared/ingredient.model";
 
 @Injectable()
 export class ShoppingListService {
+  private ingredients : Ingredient[] = [
+    new Ingredient('Apples', 5),
+    new Ingredient('Tomatoes', 10)
+  ];
 
+  getIngredients(){
+    return this.ingredients.slice();
+  }
 }
