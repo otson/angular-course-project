09750202f6eb7b5be3f67afa3cc2bb455b88c7e9
otson <59139705+otson@users.noreply.github.com>
2021-08-15 10:18:48 +0300

    move recipe list to service

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,5 +1,6 @@
 import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 import {Recipe} from "../recipe.model";
+import {RecipeService} from "../recipe.service";
 
 @Component({
   selector: 'app-recipe-list',
@@ -7,14 +8,14 @@ import {Recipe} from "../recipe.model";
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  recipes: Recipe[] =  [
-    new Recipe('A Test Recipe', 'This is a test', 'https://images.twinkl.co.uk/tw1n/image/private/t_630_eco/image_repo/4e/7b/au-t2-e-5093-pancake-recipe-english_ver_5.webp')
-  ];
+  recipes: Recipe[];
+
   @Output() recipeWasSelected = new EventEmitter<Recipe>();
 
-  constructor() { }
+  constructor(private recipeService: RecipeService) { }
 
   ngOnInit(): void {
+    this.recipes = this.recipeService.getRecipes();
   }
 
   onRecipeSelected(recipe: Recipe) {
--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,6 +1,13 @@
 import {Injectable} from "@angular/core";
+import {Recipe} from "./recipe.model";
 
 @Injectable()
 export class RecipeService {
+  private recipes: Recipe[] =  [
+    new Recipe('A Test Recipe', 'This is a test', 'https://images.twinkl.co.uk/tw1n/image/private/t_630_eco/image_repo/4e/7b/au-t2-e-5093-pancake-recipe-english_ver_5.webp')
+  ];
 
+  getRecipes() {
+    return this.recipes.slice(); // new copy
+  }
 }
