ae408d5c3921522f00671a3710d97560e3eac759
otson <59139705+otson@users.noreply.github.com>
2021-08-15 10:28:15 +0300

    move recipe selected event to recipe service

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,11 +1,14 @@
-import {Injectable} from "@angular/core";
+import {Injectable, EventEmitter} from "@angular/core";
 import {Recipe} from "./recipe.model";
 
 @Injectable()
 export class RecipeService {
   private recipes: Recipe[] =  [
-    new Recipe('A Test Recipe', 'This is a test', 'https://images.twinkl.co.uk/tw1n/image/private/t_630_eco/image_repo/4e/7b/au-t2-e-5093-pancake-recipe-english_ver_5.webp')
+    new Recipe('A Test Recipe', 'This is a test', 'https://images.twinkl.co.uk/tw1n/image/private/t_630_eco/image_repo/4e/7b/au-t2-e-5093-pancake-recipe-english_ver_5.webp'),
+    new Recipe('A Test Recipe 2', 'This is an another test', null)
   ];
+
+  recipeSelected = new EventEmitter<Recipe>();
 
   getRecipes() {
     return this.recipes.slice(); // new copy
